50c4121b9259283013f3b6e0e0036d80895a268b
Romain Fromi <romain.fromi@gmail.com>
2025-07-03 16:20:15 +0200

    Initial commit

---
+++ app/src/GameDisplay.tsx
@@ -0,0 +1,20 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import { GameTable, GameTableNavigation } from '@gamepark/react-game'
+import { PlayerPanels } from './panels/PlayerPanels'
+
+export function GameDisplay() {
+  const margin = { top: 7, left: 0, right: 30, bottom: 0 }
+  return (
+    <>
+      <GameTable xMin={-50} xMax={50} yMin={-30} yMax={30} margin={margin} css={process.env.NODE_ENV === 'development' && tableBorder}>
+        <GameTableNavigation />
+        <PlayerPanels />
+      </GameTable>
+    </>
+  )
+}
+
+const tableBorder = css`
+  border: 1px solid white;
+`
