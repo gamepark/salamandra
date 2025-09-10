ed17cb6db8f97d9d11fe12b1acc8780b23123dbf
Patrick Beaume <p.beaume@gmail.com>
2025-09-10 16:56:24 +0200

    remove console log

--- rules/src/rules/CheckPassAndEmptyPlacesRule.ts
+++ rules/src/rules/CheckPassAndEmptyPlacesRule.ts
@@ -21,7 +21,6 @@ export class CheckPassAndEmptyPlacesRule extends PlayerTurnRule {
       return [this.startSimultaneousRule(RuleId.PrepareNextRound)]
     }
     if (playersWhoPassed.includes(this.player)) {
-      console.log(this.player, playersWhoPassed)
       return [this.startRule(RuleId.CheckAndUseScrollTokens)]
     }
 
