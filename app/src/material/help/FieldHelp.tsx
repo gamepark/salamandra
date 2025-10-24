f41d8771bddb5d6bcee0a43e8096ce61a545335d
Romain Fromi <romain.fromi@gmail.com>
2025-10-24 11:14:42 +0200

    Fix field tiles cost

--- app/src/material/help/FieldHelp.tsx
+++ app/src/material/help/FieldHelp.tsx
@@ -1,32 +1,24 @@
-import { css } from '@emotion/react'
 import { MaterialHelpProps, Picture } from '@gamepark/react-game'
 import { Cost, CostType } from '@gamepark/salamandra/material/Cost'
 import { fieldData, FieldTile } from '@gamepark/salamandra/material/FieldTile'
 import { times } from 'es-toolkit/compat'
-import { FC, HTMLAttributes } from 'react'
-import { Trans } from 'react-i18next'
+import { FC } from 'react'
+import { Trans, useTranslation } from 'react-i18next'
 import { crystalTokenDescription } from '../CrystalTokenDescription'
 import { potionImages, primaryResourceImages } from './utils'
 
 export const FieldHelp: FC<MaterialHelpProps> = (props) => {
+  const { t } = useTranslation()
   const { item } = props
   if (!item.id) return null
   const cost = fieldData[item.id as FieldTile].cost
   const isFree = cost.length === 0
   return (
-    <div css={descriptionCss}>
-      <h4>Tuile Champ</h4>
-      <div css={costContainerCss}>
-        <span> Cost: </span>
-        <br />
-        {isFree && <Trans defaults="help.field.free" />}
-        {!isFree && (
-          <div css={imagesContainerCss}>
-            <CostDisplay resources={fieldData[item.id as FieldTile].cost} />
-          </div>
-        )}
-      </div>
-    </div>
+    <>
+      <h2>{isFree ? t('help.field.start') : t('help.field')}</h2>
+      {!isFree && <p><Trans i18nKey="help.field.cost" components={{ cost: <CostDisplay resources={fieldData[item.id as FieldTile].cost}/> }}/></p>}
+      {/*<p><Trans i18nKey="help.field.bonus" components={{ bonus: <></> }}/></p>*/}
+    </>
   )
 }
 
@@ -34,14 +26,11 @@ type CostDisplayProps = {
   resources: Cost[]
 }
 
-const CostDisplay: FC<CostDisplayProps> = (props) => {
-  const { resources } = props
+const CostDisplay = ({ resources }: CostDisplayProps) => {
   const images = resources.flatMap((cost: Cost) => getImages(cost))
   return (
     <>
-      {images.map((image: string, index: number) => (
-        <Picture key={index} picture={{ css: pictureCss } as HTMLAttributes<HTMLElement>} src={image} />
-      ))}
+      {images.map((image, index) => <Picture key={index} src={image}/>)}
     </>
   )
 }
@@ -57,40 +46,3 @@ const getImages = (cost: Cost) => {
 
   return potionImages[cost.potion]
 }
-
-const descriptionCss = css`
-  min-height: 100%;
-  width: 100%
-  overflow: hidden;
-
-  > h4 {
-    margin: 0;
-    text-transform: uppercase;
-    text-align: center;
-    padding: 0.3em;
-  }
-`
-
-const pictureCss = css`
-  height: 1em;
-  border-radius: 1em;
-  overflow: hidden;
-  font-size: 1.5em;
-  > img {
-    top: 0 !important;
-    height: 1em;
-  }
-`
-
-const costContainerCss = css`
-  margin-left: 0.4em;
-  margin-top: 0.2em;
-  font-size: 0.8em;
-  text-decoration: underline;
-`
-
-const imagesContainerCss = css`
-  margin-top: 0.2em;
-  align-items: center;
-  display: flex;
-`
